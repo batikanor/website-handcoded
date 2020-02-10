26fc95731e0dd1d5c649b99ef50c71ddfb719a3d
batikanor <57166099+batikanor@users.noreply.github.com>
2020-02-10 17:49:18 +0300

    added index, empty cv page, css and js for nav.

---
+++ navscript.js
@@ -0,0 +1,21 @@
+
+document.querySelector(".menuIcon").onclick = function(){
+    //This is an anonymous function
+    
+    //x is true if its now turned on
+    let x = document.querySelector(".navLinks").classList.toggle("navActive");
+    if (x){
+        document.querySelectorAll(".navLinks li").forEach((element, ind) => {
+            setTimeout(function(){element.style.opacity = "1";}, ind*300);   
+        });
+    }else{
+        document.querySelectorAll(".navLinks li").forEach((element, ind) => {
+            setTimeout(function(){element.style.opacity = "0";}, ind*300);
+            //console.log(ind, ind*300);
+        });
+    }
+    
+  
+   
+}
+//window.setInterval(function(){alert("You've been on this window for 1000 seconds");}, 100000);
